Partial fix for #4 and #6, also fix MCC calculation and add Model.reset()

benchmark.py
@@ -14,20 +14,27 @@ class Classification(nn.Module):
 
 def cola(filepath: str):
     data = []
-    with open(filepath, 'r', encoding = 'utf-8') as f:
-        for line in f:
-            parts = line.strip().split('\t')
-            if len(parts) == 4: data.append((parts[3].encode('utf-8'), int(parts[1])))
+
+    try:
+        with open(filepath, 'r', encoding = 'utf-8') as f:
+            for line in f:
+                parts = line.strip().split('\t')
+                if len(parts) == 4: data.append((parts[3].encode('utf-8'), int(parts[1])))
+
+    except FileNotFoundError: pass
+    
     return data
 
 def mcc(tp, tn, fp, fn):
     import math
     denominator = math.sqrt((tp + fp) * (tp + fn) * (tn + fp) * (tn + fn))
-    return (tp * tn - fp * fn) / denominator if denominator != 0 else 0.0
 
-def run():
+    score = (tp * tn - fp * fn) / denominator if denominator != 0 else 0.0
+    return score * 100
+
+def run(path: str):
     model = Model(dim = 512, layers = 16, temp = 0.75, lr = 5e-4)
-    model.load('smaller-4.5m.safetensors')
+    model.load(path)
     model.freeze()
 
     head = Classification(model.dim)
@@ -35,7 +42,11 @@ def run():
 
     data = cola('CoLA/original/raw/in_domain_train.tsv')
 
-    def l(params, state: mx.array, target: int):
+    if data == []:
+        print('invalid CoLA dataset.')
+        return
+
+    def lossfn(params, state: mx.array, target: int):
         head.update(params)
         choice = head(state)
 
@@ -48,17 +59,21 @@ def run():
         dummies = [mx.zeros((model.dim, )) for _ in range(model.layercount)]
         tp, tn, fp, fn = 0, 0, 0, 0
         
-        for i, (bytes, label) in enumerate(data):
+        for i, (b_s, label) in enumerate(data):
+            model.reset()
+
             final = None
-            for b in bytes:
-                x = model.encoder(mx.array(b))
+            for b in b_s:
+                enc = model.encoder(mx.array(b))
+                x = enc
+
                 for j, layer in enumerate(model.layers):
-                    x, state, _ = layer(x, dummies[j])
+                    x, state, _ = layer(enc, x, dummies[j])
                     layer.states = mx.stop_gradient(state)
-                
+
                 final = model.layers[-1].states
 
-            (_, choice), grads = mx.value_and_grad(l, argnums = 0)(head.trainable_parameters(), final, label)
+            (_, choice), grads = mx.value_and_grad(lossfn, argnums = 0)(head.trainable_parameters(), final, label)
 
             headopt.update(head, grads)
             mx.eval(head.parameters(), headopt.state)
@@ -71,9 +86,9 @@ def run():
 
             score = mcc(tp, tn, fp, fn)
 
-            if i > 0 and i % 500 == 0: print(f'{i + 1}: TP, TN, FP, FN | {tp}, {tn}, {fp}, {fn} ({score})')
+            if i > 0 and i % 500 == 0: print(f'{i}: T+ {tp}, T- {tn}, F+ {fp}, F- {fn} ({score:.4f})')
 
-        print(f'{i + 1}: TP, TN, FP, FN | {tp}, {tn}, {fp}, {fn} ({score})')
+        print(f'{i}: T+ {tp}, T- {tn}, F+ {fp}, F- {fn} ({score:.4f})')
 
 if __name__ == '__main__':
-    run()
+    run('experimental-4.5m.safetensors')

main.py
@@ -58,6 +58,14 @@ class Model(nn.Module):
         temp = mx.maximum(0.1, self.temp * (1.0 - self.temp * entropy)).item()
         return mx.random.categorical(output / temp)
 
+    def reset(self):
+        for layer in self.layers:
+            layer.decay = mx.zeros((self.dim, ))
+            layer.states = mx.zeros((self.dim, ))
+
+            layer.decaytrace = mx.zeros((self.dim, ))
+            layer.embedtrace = mx.zeros((256, self.dim))
+
     def step(self, c: mx.array, dummies: mx.array):
         enc = self.encoder(c)
         x = enc
@@ -241,5 +249,5 @@ class Runtime:
 
 if __name__ == '__main__':
     # Runtime(path = 'larger-130m.safetensors', threshold = 0.35, dim = 2048, layers = 32, temp = 0.75, lr = 5e-4)()
-    Runtime(path = 'experimental2-4.5m.safetensors', threshold = 0.35, dim = 512, layers = 16, temp = 0.75, lr = 5e-4)()
+    Runtime(path = 'experimental-4.5m.safetensors', threshold = 0.35, dim = 512, layers = 16, temp = 0.75, lr = 5e-4)()
     # param count = (256 * dim) + (dim * dim + dim * 2 + dim) + (256 * dim + dim + 1)